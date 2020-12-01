0b5ec6da4be185f5e4327d9698b41eb93e524a6c
moshemoshe137 <mosherubin137@gmail.com>
2020-12-01 17:18:38 -0600

    add back the useless test code for merge testing and add one more line

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,13 @@
 import itertools
 
 
-# here are some changes to my project
-def get_nothing(x):
-    if x > 0:
-        return
-    else:
-        pass
-# one extra line
+# # here are some changes to my project
+# def get_nothing(x):
+#     if x > 0:
+#         return
+#     else:
+#         pass
+
 
 class Cell:
 
